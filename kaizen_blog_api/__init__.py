6059349cbd26bcd98417f2dbd97e366668f3b729
Alfonso <amlluch@gmail.com>
2021-03-29 14:21:25 +0100

    small fixes

--- kaizen_blog_api/__init__.py
+++ kaizen_blog_api/__init__.py
@@ -14,7 +14,7 @@ di[Logger] = create_logger(os.getenv("LOG_LEVEL", "INFO"))
 di["dynamo_db"] = boto3.resource("dynamodb", region_name=environ.get("AWS_REGION", "eu-west-1"))
 
 di["posts_table"] = di["dynamo_db"].Table(environ.get("POSTS_TABLE", "posts"))
-di["comments_table"] = di["dynamo_db"].Table(environ.get("POSTS_TABLE", "comments"))
+di["comments_table"] = di["dynamo_db"].Table(environ.get("COMMENTS_TABLE", "comments"))
 di["sns_client"] = boto3.client("sns", region_name=environ.get("AWS_REGION", "eu-west-1"))
 di["ses_client"] = boto3.client("ses", region_name=environ.get("AWS_REGION", "eu-west-1"))
 
